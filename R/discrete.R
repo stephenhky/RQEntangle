bdbec066b5d13a2ddcfd01b59484b66d85875c00
stephenhky <stephenhky@yahoo.com.hk>
2018-03-13 17:41:14 -0400

    update matrix spec

--- R/discrete.R
+++ R/discrete.R
@@ -8,7 +8,7 @@ reduced.denmat<- function(bipartite.qubits, keep.dim=1) {
   if (keep.dim==2) bipartite.qubits<- Conj(t(bipartite.qubits))
   keepdimnum<- dim(bipartite.qubits)[1]
   tracedimnum<- dim(bipartite.qubits)[2]
-  rdenmat<- matrix(c(0)*(keepdimnum*keepdimnum), byrow=TRUE, nrow=keepdimnum)
+  rdenmat<- matrix(rep(0, keepdimnum*keepdimnum), byrow=TRUE, nrow=keepdimnum)
   for (i in 1:keepdimnum) {
     for (j in 1:keepdimnum) {
       for (k in 1:tracedimnum) {
